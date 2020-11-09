36d42272902a55f759cbd187f7225b0a7ce19751
omoya_fbit <omoya@fundaciobit.org>
2020-11-09 13:11:20 +0100

    Added ant design. Configured to allow cutomization

---
+++ craco.config.js
@@ -0,0 +1,19 @@
+//Ant Design customizations instructions at: https://ant.design/docs/react/customize-theme
+
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { "@primary-color": "#1DA57A" },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
