755b78d60049934a3fc56b8c03884a146d989347
omoya_fbit <omoya@fundaciobit.org>
2020-11-12 13:45:26 +0100

    Added spending charts (dummy data)

---
+++ src/Components/SpedingChartsContainer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import TouristArrivalsChart from "./SpendingChart";
+import { Row, Col } from "antd";
+import { ResponsiveContainer } from "recharts";
+
+const SpendingChartsContainer = (props) => {
+  const data = props.data;
+  return (
+    <Row style={{ textAlign: "center" }}>
+      <Col span={8}>
+        <div>
+          <h2 style={{ color: "#1DA57A" }}>Mallorca</h2>
+          <ResponsiveContainer>
+            <TouristArrivalsChart
+              data={data.mallorca}
+              title="Mallorca"
+            ></TouristArrivalsChart>
+          </ResponsiveContainer>
+        </div>
+      </Col>
+      <Col span={8}>
+        <div>
+          <h2 style={{ color: "#1DA57A" }}>Menorca</h2>
+          <ResponsiveContainer>
+            <TouristArrivalsChart
+              data={data.menorca}
+              title="Menorca"
+            ></TouristArrivalsChart>
+          </ResponsiveContainer>
+        </div>
+      </Col>
+      <Col span={8}>
+        <div>
+          <h2 style={{ color: "#1DA57A" }}>Eivissa i Formentera</h2>
+          <ResponsiveContainer>
+            <TouristArrivalsChart
+              data={data.ibiza_formentera}
+            ></TouristArrivalsChart>
+          </ResponsiveContainer>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default SpendingChartsContainer;
