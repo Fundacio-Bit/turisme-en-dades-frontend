0a8767f7d04e3c3d6972a7bffa8ae92815ee3152
omoya_fbit <omoya@fundaciobit.org>
2020-11-18 12:09:01 +0100

    Added loading spinner

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import MenuBar from "./MenuBar";
 import Dashboard from "./Dashboard";
+import { Spin, Space } from "antd";
+
+// TODO move Dashboard.js useEffect to Main container.js
+// TODO: then remove handleLoading from dashboard. Manage spinner directly from within this component
 
 function MainContainer() {
   const [activeSection, setActiveSection] = useState("ecs_tourist_arrivals");
+  const [loading, setLoading] = useState(true);
 
   const handleMenuSelection = (e) => {
     console.log("section", e.key);
+    setLoading(true);
     setActiveSection(e.key);
+  };
+  const handleLoading = () => {
+    setLoading(false);
   };
   return (
     <div>
@@ -15,7 +24,15 @@ function MainContainer() {
         handleMenuSelection={handleMenuSelection}
         activeSection={activeSection}
       ></MenuBar>
-      <Dashboard activeSection={activeSection}></Dashboard>
+      {loading ? (
+        <Space size="middle" style={{ marginTop: 60 }}>
+          <Spin size="large" />
+        </Space>
+      ) : null}
+      <Dashboard
+        activeSection={activeSection}
+        handleLoading={handleLoading}
+      ></Dashboard>
     </div>
   );
 }
